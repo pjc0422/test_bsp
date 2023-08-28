e49d8295925e55b7045c9f47ccb3779372445b40
pjc0422 <49198291+pjc0422@users.noreply.github.com>
2023-08-28 16:42:52 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "TestSPMModule",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TestSPMModule",
-            targets: ["TestSPMModule"]),
+        // .library(
+        //     name: "TestSPMModule",
+        //     targets: ["TestSPMModule"]),
         .library(name: "Log", targets: ["Log"])
     ],
     dependencies: [
@@ -29,13 +29,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TestSPMModule",
-            dependencies: [
-                "Alamofire",
-            ],
-            path: "TestSPMModule/Sources"
-        ),
+        // .target(
+        //     name: "TestSPMModule",
+        //     dependencies: [
+        //         "Alamofire",
+        //     ],
+        //     path: "TestSPMModule/Sources"
+        // ),
         .target(name: "Log",
                 path: "Log/Sources"),
     ]
