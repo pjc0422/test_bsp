67e2f5e9941e6a9b806cdfa36e3f87d2a1b81d0c
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-08-28 15:54:57 +0900

    swift module

--- Package.swift
+++ Package.swift
@@ -17,6 +17,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git",
                          .upToNextMajor(from: "5.0.0")),
+        .package(
+          url: "https://github.com/apple/swift-collections.git",
+          .upToNextMajor(from: "1.0.4") // or `.upToNextMinor
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
