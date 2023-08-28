832441cbde6250a6f3280820a9d2a5038d512f51
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-08-28 15:59:50 +0900

    kingfisher_test

--- Package.swift
+++ Package.swift
@@ -21,6 +21,10 @@ let package = Package(
           url: "https://github.com/apple/swift-collections.git",
           .upToNextMajor(from: "1.0.4") // or `.upToNextMinor
         ),
+        .package(
+          url: "https://github.com/onevcat/Kingfisher",
+          .upToNextMajor(from: "7.6.2") // or `.upToNextMinor
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
